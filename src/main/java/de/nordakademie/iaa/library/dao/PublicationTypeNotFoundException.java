b9e5d06c7c80f131512ad8f067971a2314c5c912
Welter <felix.welter@otto.de>
2018-10-15 17:02:56 +0200

    Add dao, service for Publication Type

---
+++ src/main/java/de/nordakademie/iaa/library/dao/PublicationTypeNotFoundException.java
@@ -0,0 +1,10 @@
+package de.nordakademie.iaa.library.dao;
+
+public class PublicationTypeNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = -6965142737973275409L;
+
+    public PublicationTypeNotFoundException(){
+        super("The publication type could not be found.");
+    }
+}
---
+++ src/main/java/de/nordakademie/iaa/library/service/PublicationTypeServiceImpl.java
@@ -0,0 +1,79 @@
+package de.nordakademie.iaa.library.service;
+
+import de.nordakademie.iaa.library.dao.PublicationTypeDAO;
+import de.nordakademie.iaa.library.model.PublicationType;
+import de.nordakademie.iaa.library.service.api.PublicationTypeService;
+
+import javax.inject.Named;
+import javax.persistence.EntityNotFoundException;
+import java.util.List;
+
+@Named
+public class PublicationTypeServiceImpl implements PublicationTypeService {
+
+    private PublicationTypeDAO dao;
+
+    public PublicationTypeServiceImpl(PublicationTypeDAO dao) {
+        this.dao = dao;
+    }
+
+    /**
+     * @param publicationType The publicationType to be persisted.
+     */
+    @Override
+    public void createPublicationType(PublicationType publicationType) {
+        dao.savePublicationType(publicationType);
+    }
+
+    /**
+     * Returns available publicationTypes
+     *
+     * @return List of publicationTypes
+     */
+    @Override
+    public List<PublicationType> listPublicationTypes() {
+        return dao.listPublicationTypes();
+    }
+
+    /**
+     * Deletes a publicationType
+     *
+     * @param id The identifier.
+     * @throws EntityNotFoundException
+     */
+    @Override
+    public void deletePublicationType(Long id) throws EntityNotFoundException {
+        PublicationType publicationType = loadPublicationType(id);
+        if (publicationType == null) {
+            throw new EntityNotFoundException();
+        }
+        dao.deletePublicationType(publicationType.getId());
+    }
+
+    /**
+     * Load a publicationType
+     *
+     * @param id The identifier.
+     * @return The requested publicationType
+     */
+    @Override
+    public PublicationType loadPublicationType(Long id) {
+        return dao.loadPublicationType(id);
+    }
+
+    /**
+     * Update a publicationType
+     *
+     * @param id
+     * @param title
+     * @throws EntityNotFoundException
+     */
+    @Override
+    public void updatePublicationType(Long id, String title) throws EntityNotFoundException {
+        PublicationType publicationType = loadPublicationType(id);
+        if (publicationType == null) {
+            throw new EntityNotFoundException();
+        }
+        publicationType.setTitle(title);
+    }
+}
