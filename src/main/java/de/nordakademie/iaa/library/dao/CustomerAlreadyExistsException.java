2be0000aa4246c0c7e90a7a3b5654c848c20965b
Welter <felix.welter@otto.de>
2018-10-16 13:46:08 +0200

    Add customer model, dao and service

---
+++ src/main/java/de/nordakademie/iaa/library/dao/CustomerAlreadyExistsException.java
@@ -0,0 +1,12 @@
+/**
+ * Author: Felix Welter
+ */
+package de.nordakademie.iaa.library.dao;
+
+public class CustomerAlreadyExistsException extends RuntimeException {
+    private static final long serialVersionUID = -460451166567912547L;
+
+    public CustomerAlreadyExistsException(){
+        super("This keyword already exists.");
+    }
+}
