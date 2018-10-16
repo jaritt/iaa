5db8f4e59c23f759f1028939ea23cf177758dd78
Jannis Baer <jannis.baer@otto.de>
2018-10-16 13:11:34 +0200

    minor bug fixes

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,8 +2,5 @@
 
 -- Test data for keywords
 INSERT INTO KEYWORD (ID, WORD) VALUES (hibernate_sequence.nextval, 1, 'Felix');
-<<<<<<< Updated upstream
 INSERT INTO KEYWORD (ID, WORD) VALUES (hibernate_sequence.nextval, 2, 'Vikash');
 INSERT INTO KEYWORD (ID, WORD) VALUES (hibernate_sequence.nextval, 3, 'Jannis');
-=======
->>>>>>> Stashed changes
