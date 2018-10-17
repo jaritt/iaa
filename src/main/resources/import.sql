962de64ae5c6eafa9d39faf8d690de4244065e38
Jannis Baer <jannis.baer@otto.de>
2018-10-17 15:34:04 +0200

    added structure to message.properties files

--- src/main/resources/import.sql
+++
@@ -1,6 +0,0 @@
---@formatter:off
-
--- Test data for keywords
-INSERT INTO KEYWORD (ID, WORD) VALUES (hibernate_sequence.nextval, 1, 'Felix');
-INSERT INTO KEYWORD (ID, WORD) VALUES (hibernate_sequence.nextval, 2, 'Vikash');
-INSERT INTO KEYWORD (ID, WORD) VALUES (hibernate_sequence.nextval, 3, 'Jannis');
--- src/main/webapp/tiles/header.jsp
+++ src/main/webapp/tiles/header.jsp
@@ -1,3 +1,3 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="s" uri="/struts-tags" %>
 
 <h1>Struts Dummy App</h1>
