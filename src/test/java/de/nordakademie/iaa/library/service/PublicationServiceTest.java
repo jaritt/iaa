f37f830316b999dfc5ba4c42fc3ae76ef4323a72
Welter <felix.welter@otto.de>
2018-10-23 16:50:38 +0200

    Improve search quality, add test for searching

--- src/test/java/de/nordakademie/iaa/library/service/PublicationServiceTest.java
+++ src/test/java/de/nordakademie/iaa/library/service/PublicationServiceTest.java
@@ -110,10 +110,77 @@ public class PublicationServiceTest extends BasicServiceTest {
     }
 
     @Test
-    public void testEagerLoading() throws NoCopyAvailable {
-        samplePublication();
-        Customer customer = new Customer("Hans", "Beiermann");
+    public void testSearch() {
+        Customer customer = new Customer("Peter", "Parker");
+        customer.setCity("SpiderTown");
+        customer.setStreet("SpiderStreet");
+        customer.setMatnr(6299L);
         customerService.createCustomer(customer);
-        lendingService.lendPublication(publicationService.listPublications().get(0), customer);
+
+        PublicationType type = new PublicationType("Fachbuch");
+        typeService.createPublicationType(type);
+        typeService.createPublicationType(new PublicationType("Hausarbeit"));
+        PublicationType type2 = new PublicationType("Lehrbuch");
+        typeService.createPublicationType(type2);
+
+        Keyword keyword = new Keyword("Softwareentwicklung");
+        keywordService.createKeyword(keyword);
+        Keyword keyword1 = new Keyword("Steuern");
+        keywordService.createKeyword(keyword1);
+        Keyword keyword3 = new Keyword("Bildung");
+        keywordService.createKeyword(keyword3);
+        Keyword keyword2 = new Keyword("Software");
+        keywordService.createKeyword(keyword2);
+
+        Publication publication = new Publication("LS-2110", "Design Patterns. Elements of Reusable Object-Oriented Software.", type, 2L);
+        publication.getKeywords().add(keyword);
+        publication.setReleaseDate(LocalDate.of(1994, 10, 31));
+        publication.setAuthor("Erich Gamma");
+        publication.setIsbn("978-0-20163-361-0");
+        publication.setPublisher("Addison-Wesley");
+        publicationService.createPublication(publication);
+
+        Publication publication2 = new Publication("LS-2200", "Steuerrecht leicht gemacht", type2, 10L);
+        publication2.getKeywords().add(keyword1);
+        publication2.getKeywords().add(keyword3);
+        publication2.setReleaseDate(LocalDate.of(2007, 10, 10));
+        publication2.setAuthor("Stephan Kudert");
+        publication2.setIsbn("978-3-87440-330-6");
+        publication2.setPublisher("Kleist");
+        publicationService.createPublication(publication2);
+
+        Publication publication3 = new Publication("LS-2120", "Microservices. Grundlagen flexibler Softwarearchitekturen.", type, 5L);
+        publication3.getKeywords().add(keyword);
+        publication3.getKeywords().add(keyword2);
+        publication3.setReleaseDate(LocalDate.of(2018, 07, 30));
+        publication3.setAuthor("Eberhard Wolff");
+        publication3.setIsbn("978-3-86490-555-1");
+        publication3.setPublisher("Dpunkt.verlag GmbH");
+        publicationService.createPublication(publication3);
+
+        try {
+            lendingService.lendPublication(publication, customer);
+        } catch (NoCopyAvailable noCopyAvailable) {
+            noCopyAvailable.printStackTrace();
+        }
+
+        // Search for ISBN und LS -> direct hits
+        assertThat(publicationService.search("978-0-20163-361-0").get(0)).isEqualTo(publication);
+        assertThat(publicationService.search("978-3-87440-330-6").get(0)).isEqualTo(publication2);
+        assertThat(publicationService.search("978-3-86490-555-1").get(0)).isEqualTo(publication3);
+        assertThat(publicationService.search("LS-2110").get(0)).isEqualTo(publication);
+        assertThat(publicationService.search("LS-2200").get(0)).isEqualTo(publication2);
+        assertThat(publicationService.search("LS-2120").get(0)).isEqualTo(publication3);
+
+        assertThat(publicationService.search("design").get(0)).isEqualTo(publication);
+        assertThat(publicationService.search("adison").get(0)).isEqualTo(publication);
+
+        assertThat(publicationService.search("Recht").get(0)).isEqualTo(publication2);
+        assertThat(publicationService.search("Kudert").get(0)).isEqualTo(publication2);
+
+        assertThat(publicationService.search("flexibel architektur").get(0)).isEqualTo(publication3);
+        assertThat(publicationService.search("wollf").get(0)).isEqualTo(publication3);
+
+        assertThat(publicationService.search("software").size()).isEqualTo(2);
     }
 }
